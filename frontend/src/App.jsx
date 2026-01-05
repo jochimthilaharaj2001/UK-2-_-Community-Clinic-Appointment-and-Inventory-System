5570478a9d9a34a96224398b7c23e42c18caf987
PrathikshaM01 <prathi8765@gmail.com>
2026-01-05 19:59:29 +0530

    My frontend changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-// src/App.jsx - SIMPLE TEST VERSION
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/';
+import DoctorDashboard from './pages/doctor/DoctorDashboard';
+import InventoryPage from './pages/pharmacist/InventoryPage';
+
+
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        React is working! 🎉
-      </h1>
-    </div>
+    <BrowserRouter>
+    <div className="p-6">
+   <Routes>
+    <Route path ="./doctor" element = {<DoctorDashboard/>}/>
+   </Routes>
+   </div>
+   </BrowserRouter>
   )
 }
