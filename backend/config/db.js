0b83932f60a1cea89f7ec09ba6ec71b60cb2c9cf
fazanfawz <fazancool2001@gmail.com>
2026-01-16 10:21:48 +0530

    backend folder structure update

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require("mysql2/promise");
-
-module.exports = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
---
+++ backend/src/routes/reportRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import {
+  inventoryReport,
+  lowStockReport,
+  dispensedReport
+} from '../controllers/reportController.js';
+
+const router = express.Router();
+
+router.get('/inventory', inventoryReport);
+router.get('/low-stock', lowStockReport);
+router.get('/dispensed', dispensedReport);
+
+export default router;
