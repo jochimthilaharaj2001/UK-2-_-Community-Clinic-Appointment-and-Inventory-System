2e53747db9a5b4ed7bb13257aab518c02a058b79
Jochim Thilaharaj <bthilaharaj@gmail.com>
2026-01-20 19:49:28 +0530

    Delete backend directory

--- backend/src/routes/pharmacistRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { loginPharmacist } from '../controllers/pharmacistController.js';
-
-const router = express.Router();
-
-router.post('/login', loginPharmacist);
-
-export default router;
